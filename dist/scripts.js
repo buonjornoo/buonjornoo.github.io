9a52765af39a4921102a228e6c9ed9cf00c2a34c
buonjornoo <40203799+buonjornoo@users.noreply.github.com>
2023-11-17 18:49:04 +0100

    small screen nav menu fix

--- dist/scripts.js
+++ dist/scripts.js
@@ -1,35 +1,67 @@
 // script.js
-document.addEventListener("DOMContentLoaded", function () {
-  // Function to dynamically update the year
-  function updateYear() {
-    document.getElementById("currentYear").innerText = new Date().getFullYear();
-  }
 
-  // Call the updateYear function when the DOM is loaded
-  updateYear();
-});
+// Function to dynamically update the year
+function updateYear() {
+  document.getElementById("currentYear").innerText = new Date().getFullYear();
+}
 
-// Navbar and Footer
-// Function to load the navigation bar and footer
+// Function to set the active link in the navbar
 function setActiveLink() {
-  // Get the current URL path
   const path = window.location.pathname;
-
-  // Extract the last directory name from the path
   const segments = path.split("/").filter(Boolean);
   let pageId = segments.length === 0 ? "index" : segments.slice(-1)[0];
+  pageId = pageId.replace(".html", "");
+  $(`a[data-page-id='${pageId}']`).addClass("active");
+}
 
-  // Set the active class on the current navigation link
-  $(`a[data-page-id=${pageId}]`).addClass("active");
+// Initialize the navbar button functionality
+function initNavbarButton() {
+  const menuButton = document.getElementById("menuButton");
+  const closeMenuButton = document.getElementById("closeMenuButton");
+  const navMenu = document.getElementById("navMenu");
+
+  if (menuButton && navMenu) {
+    menuButton.addEventListener("click", function () {
+      navMenu.classList.toggle("hidden");
+    });
+  }
+
+  if (closeMenuButton && navMenu) {
+    closeMenuButton.addEventListener("click", function () {
+      navMenu.classList.add("hidden");
+    });
+  }
+}
+
+// Function to update menu visibility based on screen width
+function updateMenuVisibility() {
+  const navMenu = document.getElementById("navMenu");
+  const menuButton = document.getElementById("menuButton");
+
+  if (window.matchMedia("(min-width: 768px)").matches) {
+    navMenu.classList.add("hidden");
+    menuButton.classList.add("hidden");
+  } else {
+    navMenu.classList.add("hidden");
+    menuButton.classList.remove("hidden");
+  }
 }
 
 // Load the navbar and footer, then set the active link
 function loadNavAndFooter() {
   $("#nav-placeholder").load("/navbar/index.html", function () {
     setActiveLink(); // Set active link after navbar is loaded
+    initNavbarButton(); // Initialize navbar button after navbar is loaded
+    updateMenuVisibility(); // Update menu visibility based on current screen width
   });
-  $("#footer-placeholder").load("/footer/index.html");
+
+  $("#footer-placeholder").load("/footer/index.html", function () {
+    updateYear();
+  });
 }
 
-// Call the function on page load
-document.addEventListener("DOMContentLoaded", loadNavAndFooter);
+// Load the navbar and footer when the DOM is fully loaded
+document.addEventListener("DOMContentLoaded", function () {
+  loadNavAndFooter();
+  window.addEventListener("resize", updateMenuVisibility);
+});
