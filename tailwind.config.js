73b105111e9f7787c07d8df72159ba9db360f92a
buonjornoo <40203799+buonjornoo@users.noreply.github.com>
2023-11-17 13:36:11 +0100

    config.js changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./dist/*.{html,js}"],
+  content: ["./dist/*.{html,js}", "./*.{html,js}"],
   theme: {
     extend: {
       backgroundImage: (theme) => ({
